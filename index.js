70ddedd6348c494e8c1b7ff32ee8408450a72666
GabrieleCosta00 <gabriele.costa.1h@gmail.com>
2023-01-24 19:28:46 +0100

    Ho aggiunto i due spartiti di CF e CTP che seguono quello che uno suona. Al momento scrivono tutto in fila e ripartono da capo, ovviamente andrà "regolamentato".

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const pianoKeys = document.querySelectorAll('.key')
+const noteTexts = document.querySelectorAll(".noteText")
 var notePlaying = null;
 var keyIsPressed = false;
+var currentIndex = 0;
+const noteNames = [ 'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
+    'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5'];
 
 pianoKeys.forEach((pianoKey, i) => {
     const number = i < 9 ? '0' + (i + 1) : (i + 1)
     const newUrl = 'Samples/Piano_sample_' + number + '.mp3'
-    pianoKey.addEventListener("mousedown", () => playSound(newUrl))
+    pianoKey.addEventListener("mousedown", () => playSound(newUrl, number))
     pianoKey.addEventListener("mouseup", () => newPossibilities(i))
     pianoKey.addEventListener("mousedown", () => clearPossibilities())
     pianoKey.addEventListener("mouseup", () => stopSound())
 })
 
-function playSound(newUrl) {
+function playSound(newUrl, number) {
+    if (currentIndex === 16) {currentIndex = 0;}
     let currentAudio = new Audio(newUrl)
     notePlaying = currentAudio;
     keyIsPressed = true;
     currentAudio.play();
+    writeNote(noteNames[Number(number)-1], currentIndex);
+    currentIndex = currentIndex + 1;
 }
 
 function stopSound() {
@@ -49,6 +56,14 @@ function newPossibilities(offset) {
         if (Math.abs(offset-i) < 4) {
             console.log("offset - i = " + Math.abs(offset-i))
             pianoKey.classList.add("possible")
+        }
+    })
+}
+
+function writeNote(note, index) {
+    noteTexts.forEach((text, i) => {
+        if (i === index) {
+            text.innerHTML = note;
         }
     })
 }
