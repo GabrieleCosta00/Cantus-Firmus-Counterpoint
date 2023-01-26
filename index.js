e873cbddb0447852db57252afcc1e8974d331edd
GabrieleCosta00 <gabriele.costa.1h@gmail.com>
2023-01-26 15:35:23 +0100

    Ho messo a posto la grafica e soprattutto aggiunto la possibilità di essere quonata non solo con il mouse, ma anche con la tastiera del proprio computer. In più ho aggiunto lo spazio che andrà dedicato al vero e proprio spartito di CF e CTP.

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ const pianoKeys = document.querySelectorAll('.key')
 const noteTexts = document.querySelectorAll('.noteText')
 const CFScore = document.getElementById('scoreCF')
 const CTPScore = document.getElementById('scoreCTP')
-var notePlayingCF = null;
-var notePlayingCTP = null;
-var indexCF = 0;
-var indexCTP = 0;
-var whichScore = "CF";
-var whichScorePt;
-var currentTransport = 0;
-var isPlaying = false;
+let notePlayingCF = null;
+let notePlayingCTP = null;
+let indexCF = 0;
+let indexCTP = 0;
+let whichScore = "CF";
+let whichScorePt;
+let currentTransport = 0;
+let isPlaying = false;
+let keyIsPressed = false;
 const playRate = 1000; // ms between the notes when playing
+let CFNotes = [0, 0, 0, 0, 0, 0, 0, 0]; // 0 -> no note; number!=0 -> a valid note
+let CTPNotes = [0, 0, 0, 0, 0, 0, 0, 0]; // 0 -> no note; number!=0 -> a valid note
 const noteNames = [ 'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
     'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',
     'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5', 'C6'];
+
+// The peace of code that places the black keys in the keyboard
+let nthBlackKey = 0;
+document.querySelectorAll('.black-key').forEach(displayBlackKeys);
+function displayBlackKeys(e, index) {
+    if (index === 2 || index === 5 || index === 7 || index === 10 || index === 12) {nthBlackKey++;}
+    e.setAttribute("style", "left: " + (37 + (nthBlackKey * 51)) + "px");
+    nthBlackKey++;
+}
 
 // If you click on the CF box you will write this score. Same for the CTP
 CFScore.addEventListener("click", () => {
@@ -41,11 +53,81 @@ pianoKeys.forEach((pianoKey, i) => {
     pianoKey.addEventListener("mouseup", () => stopSound())
 })
 
-// It plays the sound you chosen on the keyboard and write it in the CF/CTP, based on which you have chosen
+// When you press a key on the computer keyboard...
+document.addEventListener("keydown", convertPlayWriteClearPoss)
+document.addEventListener("keyup", stopSound)
+document.addEventListener("keyup", newPossibilitiesKey)
+
+function newPossibilitiesKey(e) {
+    let i = (Number(converter(e)) - 1);
+    newPossibilities(i);
+}
+
+// The function that does the same operations of the "pianoKeys.forEach..." of the previous lines,
+// after converting the character to the corresponding piano key
+function convertPlayWriteClearPoss(character) {
+    if (!keyIsPressed) {
+        keyIsPressed = true;
+        let number = converter(character)
+        if (number !== '0') {
+            const newUrl = 'Samples/Piano_sample_' + number + '.mp3'
+            playSoundAndWrite(newUrl, number)
+            clearPossibilities()
+        }
+    }
+}
+
+// The function that convert the key on the computer keyboard to the corresponding piano key
+function converter(ch) {
+    let num = '0';
+    switch (ch.key) {
+        case 'z': {num = '01'; break;}
+        case 's': {num = '02'; break;}
+        case 'x': {num = '03'; break;}
+        case 'd': {num = '04'; break;}
+        case 'c': {num = '05'; break;}
+        case 'v': {num = '06'; break;}
+        case 'g': {num = '07'; break;}
+        case 'b': {num = '08'; break;}
+        case 'h': {num = '09'; break;}
+        case 'n': {num = '10'; break;}
+        case 'j': {num = '11'; break;}
+        case 'm': {num = '12'; break;}
+        case ',': {num = '13'; break;}
+        case 'l': {num = '14'; break;}
+        case '.': {num = '15'; break;}
+        case 'ò': {num = '16'; break;}
+        case '-': {num = '17'; break;}
+        case 'q': {num = '18'; break;}
+        case '2': {num = '19'; break;}
+        case 'w': {num = '20'; break;}
+        case '3': {num = '21'; break;}
+        case 'e': {num = '22'; break;}
+        case '4': {num = '23'; break;}
+        case 'r': {num = '24'; break;}
+        case 't': {num = '25'; break;}
+        case '6': {num = '26'; break;}
+        case 'y': {num = '27'; break;}
+        case '7': {num = '28'; break;}
+        case 'u': {num = '29'; break;}
+        case 'i': {num = '30'; break;}
+        case '9': {num = '31'; break;}
+        case 'o': {num = '32'; break;}
+        case '0': {num = '33'; break;}
+        case 'p': {num = '34'; break;}
+        case '\'': {num = '35'; break;}
+        case 'è': {num = '36'; break;}
+        case '+': {num = '37'; break;}
+    }
+    return num;
+}
+
+// It plays the sound you chosen on the keyboard and write it in the CF/CTP (in both the box and the vector of notes), based on which you have chosen
 function playSoundAndWrite(newUrl, number) {
     if (whichScore === "CF") {
         if (indexCF >= 8) {indexCF = 0;}
         writeNote(noteNames[Number(number)-1], indexCF);
+        CFNotes[indexCF] = Number(number);
         indexCF = indexCF + 1;
         let currentAudio = new Audio(newUrl)
         notePlayingCF = currentAudio;
@@ -54,6 +136,7 @@ function playSoundAndWrite(newUrl, number) {
     else {
         if (indexCTP >= 8) {indexCTP = 0}
         writeNote(noteNames[Number(number)-1], indexCTP+8);
+        CTPNotes[indexCTP] = Number(number);
         indexCTP = indexCTP + 1;
         let currentAudio = new Audio(newUrl)
         notePlayingCTP = currentAudio;
@@ -80,6 +163,7 @@ function playSoundAndColourKey(noteName) {
 
 // It stops all the sound gradually (decreasing the volume) and clear the colour of the keyboard
 function stopSound() {
+    if (keyIsPressed) {keyIsPressed = false}
     if(notePlayingCF !== null) {
         decreaseVolume(notePlayingCF);
         notePlayingCF = null;
@@ -111,12 +195,13 @@ function clearPossibilities() {
     pianoKeys.forEach(pianoKey => {
         pianoKey.classList.remove("possible")
     })
+    console.log("cleared")
 }
 
 // Here goes the rules for the choise of the next note (it add a colour)
 function newPossibilities(offset) {
     pianoKeys.forEach((pianoKey, i) => {
-        if (Math.abs(offset-i) < 4) {
+        if (Math.abs(Number(offset)-i) < 4) {
             pianoKey.classList.add("possible")
         }
     })
@@ -146,11 +231,11 @@ function transportPlay() {
 function playing() {
     noteTexts.forEach((note, i) => {
         stopSound();
-        if (i === currentTransport) {playSoundAndColourKey(note.innerHTML);}
+        if (i === currentTransport) {playSoundAndColourKey(note.innerHTML)}
     })
     noteTexts.forEach((note, i) => {
         stopSound();
-        if (i === currentTransport + 8) {playSoundAndColourKey(note.innerHTML);}
+        if (i === currentTransport + 8) {playSoundAndColourKey(note.innerHTML)}
     })
     currentTransport++;
     if (currentTransport === 8) {
@@ -169,5 +254,4 @@ function transportStop() {
     stopSound();
     isPlaying = false;
     currentTransport = 0;
-    setTimeout(() => {console.log("Stop unselected")}, 500);
 }
